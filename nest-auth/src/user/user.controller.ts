5b62ba694e67eb429490332a4dbd6e6bbae5a7c2
fabo <fabo.dev.email@gmail.com>
2023-11-21 20:02:22 +0100

    #5
    user service, register user

--- nest-auth/src/user/user.controller.ts
+++ nest-auth/src/user/user.controller.ts
@@ -1,10 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import * as bcryptjs from "bcryptjs";
+
+import { UserService } from './user.service';
 
 @Controller('api')
 export class UserController {
+	constructor(
+		private userService: UserService
+	) { }
 
+	// TODO type body
 	@Post('register')
-	register(@Body() body: any){
-		return body
+	async register(@Body() body: any) {
+
+		if (body.password !== body.password_confirm) {
+			throw new BadRequestException('Password does not match');
+		}
+		return this.userService.save({
+			first_name: body.first_name,
+			last_name: body.last_name,
+			email: body.email,
+			password: await bcryptjs.hash(body.password, 12),
+		});
 	}
 }
---
+++ nest-auth/src/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+	constructor(@InjectRepository(User) protected readonly userRepository: Repository<User>) { }
+
+	// TODO body type
+	async save(body) {
+		return this.userRepository.save(body, {});
+	}
+}

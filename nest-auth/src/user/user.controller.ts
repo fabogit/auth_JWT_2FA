f8e9fc8c9a0f0a8bf80afe09ccb02db9ac52ccf4
fabo <fabo.dev.email@gmail.com>
2024-03-14 20:05:32 +0100

    #8
    jwt: access and refresh token

--- nest-auth/src/user/user.controller.ts
+++ nest-auth/src/user/user.controller.ts
@@ -1,19 +1,28 @@
-import { BadRequestException, Body, Controller, NotFoundException, Post } from '@nestjs/common';
+import {
+	BadRequestException,
+	Body,
+	Response,
+	Controller,
+	NotFoundException,
+	Post,
+} from '@nestjs/common';
 // import * as bcrypt from "bcrypt";
-import { hash, compare } from "bcrypt";
+import { hash, compare } from 'bcrypt';
+import { Response as Res } from 'express';
 
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from './user.service';
 
 @Controller('api')
 export class UserController {
 	constructor(
-		private userService: UserService
+		private userService: UserService,
+		private jwtService: JwtService,
 	) { }
 
 	// TODO type body
 	@Post('register')
 	async register(@Body() body: any) {
-
 		if (body.password !== body.password_confirm) {
 			throw new BadRequestException('Passwords do not match');
 		}
@@ -29,6 +38,7 @@ export class UserController {
 	async login(
 		@Body('email') email: string,
 		@Body('password') password: string,
+		@Response({ passthrough: true }) response: Res,
 	) {
 		const user = await this.userService.findOne(email);
 		if (!user) {
@@ -40,7 +50,20 @@ export class UserController {
 			throw new BadRequestException('Invalid credentials');
 		}
 
-		return user;
-	}
+		const accessToken = await this.jwtService.signAsync(
+			{ id: user.id },
+			{ expiresIn: '30s' },
+		);
+		const refreshToken = await this.jwtService.signAsync({ id: user.id });
 
+		response.cookie('refreshToken', refreshToken, {
+			// helps mitigate the risk of client-side scripts accessing the protected cookie
+			// cookie cannot be accessed through the client-side script
+			httpOnly: true,
+			//  1week
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+		});
+
+		return { token: accessToken };
+	}
 }
--- nest-auth/src/user/user.module.ts
+++ nest-auth/src/user/user.module.ts
@@ -4,10 +4,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
 	imports: [
-		TypeOrmModule.forFeature([User])
+		TypeOrmModule.forFeature([User]),
+		JwtModule.register({
+			// global: true,
+			secret: 'jwtConstants.secret',
+			signOptions: {
+				// refreshToken duration
+				expiresIn: '1w'
+			},
+		}),
 	],
 	controllers: [UserController],
 	providers: [UserService]

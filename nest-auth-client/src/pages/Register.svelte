b7e03584febad6404b866dad924cfd2c8ea94cfe
fabogit <fabo.dev.email@gmail.com>
2025-02-09 08:53:45 +0100

    #20
    Enabled routing for Login and Register pages

---
+++ nest-auth-client/src/pages/Register.svelte
@@ -0,0 +1,32 @@
+<main class="form-signin w-100 m-auto">
+  <form>
+    <h1 class="h3 mb-3 fw-normal">Please Register</h1>
+
+    <div class="form-floating">
+      <input class="form-control" id="first-name" placeholder="First Name" />
+      <label for="first-name">First Name</label>
+    </div>
+
+    <div class="form-floating">
+      <input class="form-control" id="last-name" placeholder="Last Name" />
+      <label for="last-name">Last Name</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="email" placeholder="name@example.com" />
+      <label for="email">Email address</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="password" class="form-control" id="password" placeholder="Password" />
+      <label for="password">Password</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="password" class="form-control" id="password-confirm" placeholder="Password Confirm" />
+      <label for="password-confirm">Password Confirm</label>
+    </div>
+
+    <button class="btn btn-primary w-100 py-2" type="submit">Submit</button>
+  </form>
+</main>
